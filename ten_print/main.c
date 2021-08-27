8d938d364aa758ce7bfb93c568ad61f6c11d6a4b
RandomPhile <richifurlan@gmail.com>
2021-08-27 09:45:56 +0200

    nuovi progetti (incompleti)

--- ten_print/main.c
+++ ten_print/main.c
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#include "graphics.h"
+#include<conio.h>
+
+int main(int argc, char const *argv[])
+{
+   int gd = DETECT, gm;
+
+   initgraph(&gd, &gm, "C:\\TC\\BGI");
+       
+   getch();
+   closegraph();
+   return 0;
+}
